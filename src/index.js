1e32b3533a31238b4677db40867d2599a09af561
Vladislav Shulyugin <vladislav.shulyugin@gmail.com>
2017-02-14 19:41:14 +0300

    Custom messages

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import _presence from './pure/presence'
 import _regexp from './pure/regexp'
 import _email from './pure/email'
 import fromPure from './fromPure'
+import withMessage from './withMessage'
 import validate from './validate'
 import depends from './depends'
 
 export default validate
-export { depends, fromPure }
+export { depends, fromPure, withMessage }
 export const presence = fromPure(_presence, 'presence')
 export const regexp = fromPure(_regexp, 'regexp')
 export const email = fromPure(_email, 'email')
---
+++ src/withMessage.js
@@ -0,0 +1,11 @@
+import path from 'ramda/src/path'
+import always from 'ramda/src/always'
+
+const withMessage = (validator, name) => fieldName => form => {
+  if (name !== fieldName) return form
+  const { value } = path(['fields', fieldName], form)
+  const { valid, message } = validator(value)
+  return fromPure(always(valid), fieldName)(fieldName, {}, message)(form)
+}
+
+export default withMessage
